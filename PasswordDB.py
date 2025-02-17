f6748579b1999a781d85babd6b86946ceb1afd72
kassie <kass.squawk@gmail.com>
2025-02-17 15:41:39 +0000

    some comments

--- PasswordDB.py
+++ PasswordDB.py
@@ -7,19 +7,20 @@ class PasswordDB:
         self.passwordHashes: Dict[str, int] = pickle.load(open("data/passwordHashes.dat", "rb"))
         
     def add(self, username: str, password: str) -> None:
-        self.passwordHashes[username] = hash(password)
+        self.passwordHashes[username] = hash(password)  # Create entry in dictionary for given username and password.
         
         self.saveChanges()
         
     def delete(self, username: str) -> None:
-        self.passwordHashes.pop(username, None)
+        self.passwordHashes.pop(username, None)  # Remove key from dictionary.
         
         self.saveChanges()
         
     def passwordIsCorrect(self, username: str, password: str) -> bool:
         passwordHash = hash(password)
         
-        return self.passwordHashes[username] == passwordHash
+        # Assumes username exists. Validation is always done before this function is called.
+        return self.passwordHashes[username] == passwordHash  # Returns true if password hashes to same value.
         
     def saveChanges(self) -> None:
         with open("data/passwordHashes.dat", "wb") as file:
