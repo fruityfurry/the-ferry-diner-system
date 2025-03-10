d04fd63ee2acce4082ff0589add9159d3b16ee1b
kassie <kass.squawk@gmail.com>
2025-03-10 14:46:27 +0000

    main.py changed to release version

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 """Entry point of program."""
 
-# import Login
-# Login.Login()  # Run login window. no need for mainloop as that is done in Login's __init__.
-#                # This same technique is used for all windows in this program.
+import Login
 
-import AdminMenu
-from EmployeeDB import EmployeeDB
-
-employees = EmployeeDB()
-user = employees.getByUsername("colinr83")  # Admin user.
-AdminMenu.AdminMenu(user)  # Skip straight to admin menu. For testing purposes only.
+Login.Login()  # Run login window. no need for mainloop as that is done in Login's __init__.
+               # This same technique is used for all windows in this program.
