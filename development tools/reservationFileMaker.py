dcf2f92d66d2b1e7b2e69888d9abf1ea698587b6
kassie <kass.squawk@gmail.com>
2025-03-11 19:51:13 +0000

    add some reservations by default, still need to add their corresponding orderdb entries

--- development tools/reservationFileMaker.py
+++ development tools/reservationFileMaker.py
@@ -3,8 +3,12 @@ sys.path.insert(1, "C:/Users/kasss/Documents/My Code/Python/The Ferry Diner")  #
 # This is an absolute file path, so it won't work on anyone's system but mine. This is fine though since this is just
 # a development tool.
 import pickle
+from Reservation import Reservation
 
-reservations = []
+reservations = [
+    Reservation(0, 2, "jasonizcool", "18:30", 2),
+    Reservation(1, 0, "lauraaaa", "19:00", 1)
+]
 
 with open("data/reservations.dat", "wb+") as file:
     pickle.dump(reservations, file)
