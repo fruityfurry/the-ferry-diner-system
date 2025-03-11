47b31e34ec3e326a27abd9a059d6e657b2a97837
kassie <kass.squawk@gmail.com>
2025-03-11 19:37:23 +0000

    extra comments explaining sys path insert in dev tools

--- development tools/employeeFileMaker.py
+++ development tools/employeeFileMaker.py
@@ -1,5 +1,7 @@
 import sys
 sys.path.insert(1, "C:/Users/kasss/Documents/My Code/Python/The Ferry Diner")  # Allows import of files in parent directory.
+# This is an absolute file path, so it won't work on anyone's system but mine. This is fine though since this is just
+# a development tool.
 import pickle
 from Employee import Employee
 
